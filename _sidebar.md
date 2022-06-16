95c10c83ec1586d3c90a9ccfd36f16fe839ed780
xuyongfei <xuyongfei@dingdingzn.com>
2022-06-16 16:40:25 +0800

    feat: 换用vuerpress

--- _sidebar.md
+++
@@ -1,4 +0,0 @@
-* 个人
-	* [介绍](zh-cn/aboutme)
-* 项目
-	* [叮叮充电桩](zh-cn/project/dingding) 
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'xyfei',
+	description: 'xyfei的个人网站',
+	base: '/',
+	themeConfig: {
+    sidebar: [
+      '../profile/',
+			'../skill/',
+			'../work/',
+    ]
+  }
+}
